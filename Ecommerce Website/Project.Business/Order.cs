95c0ec690bace654322819868534ab05b0492028
Qabas ALJUBORY <80683467+QabasAljubory@users.noreply.github.com>
2021-06-06 23:20:28 +0300

    Project

---
+++ Ecommerce Website/Project.Business/Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Business
+{
+    public class Order
+    {
+        public int OrderID { get; set; }
+        public int CustomeFK { get; set; }
+        public int CartFK { get; set; }
+       
+       
+
+    }
+}
